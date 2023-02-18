a3b9a5bedd155d3da5e498f19206d0166fa8af89
조영준 <joyoungjun8590@hotmail.com>
2023-02-18 23:25:29 +0900

    Group 엔티티 생성

---
+++ src/main/java/com/example/demo/domain/entity/Group.java
@@ -0,0 +1,22 @@
+package com.example.demo.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "CUSTOM_GROUP")
+public class Group {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long groupId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "FRIEND_ID")
+    private Friend friend;
+
+    @Column(name = "GROUP_NAME")  private String name;
+    @Column(name = "GROUP_UUID") private String uuid;
+}
