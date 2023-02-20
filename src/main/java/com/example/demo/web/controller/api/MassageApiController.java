8c17c54a6c05bff73c3159d6cbab3ebe16f119c2
최준혁 <cwnsugr13579@gmail.com>
2023-02-20 17:40:57 +0900

    메세지 보낼 대상을 위해 로그인 유저의 친구목록을 불러오는 API 구현. 임시로 MessageApiController 에 구현하였으나 나중에 다른 Controller 로 옮길 필요가 있음. 임시로 DTO <-> Entity 변환을 컨트롤러에서 처리했는데 나중에 Service 단으로 옮길 필요가 있음.

---
+++ src/main/java/com/example/demo/web/controller/api/MassageApiController.java
@@ -0,0 +1,38 @@
+package com.example.demo.web.controller.api;
+
+import com.example.demo.domain.dto.FriendDto;
+import com.example.demo.domain.entity.Friend;
+import com.example.demo.domain.entity.User;
+import com.example.demo.domain.repository.FriendRepository;
+import com.example.demo.domain.service.kakao.KakaoService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/message")
+public class MassageApiController {
+  private final KakaoService kakaoService;
+  private final FriendRepository friendRepository;
+  @GetMapping("/friendlist")
+  public List<FriendDto> getFriendList(User user) {
+    List<Friend> friendList = friendRepository.findByUserId(user.getId());
+
+    ModelMapper modelMapper = new ModelMapper();
+    List<FriendDto> result = modelMapper.map(friendList, new TypeToken<List<FriendDto>>() {}.getType());
+
+    for (FriendDto friendDto : result) {
+      System.out.println(friendDto);
+    }
+
+    return result;
+  }
+
+
+}
