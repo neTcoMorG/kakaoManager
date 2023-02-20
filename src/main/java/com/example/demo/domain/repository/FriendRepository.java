acb6ad05207abec1b4104383460d20e478a25b9f
최준혁 <cwnsugr13579@gmail.com>
2023-02-20 17:37:45 +0900

    특정 유저의 친구목록을 불러오기 위해 FriendRepository 에 findByUserId 메소드 추가.

--- src/main/java/com/example/demo/domain/repository/FriendRepository.java
+++ src/main/java/com/example/demo/domain/repository/FriendRepository.java
@@ -3,9 +3,11 @@ package com.example.demo.domain.repository;
 import com.example.demo.domain.entity.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
     Optional<Friend> findByUuid(String uuid);
+    List<Friend> findByUserId(Long userId);
 }
