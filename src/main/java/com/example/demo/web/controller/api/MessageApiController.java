177245bbe6a607813672ab70f182349b8a57407e
Haisia <cwnsgur13579@gmail.com>
2023-02-20 23:45:30 +0900

    메세지 전송 컨트롤러 구현. 메세지 템플릿에 따라 API를 분리하였음. 입력 파라미터의 검증 기능 구현이 필요.

--- src/main/java/com/example/demo/web/controller/api/MessageApiController.java
+++ src/main/java/com/example/demo/web/controller/api/MessageApiController.java
@@ -1,16 +1,24 @@
 package com.example.demo.web.controller.api;
 
 import com.example.demo.domain.dto.FriendDto;
+import com.example.demo.domain.service.kakao.message.json.feed.FeedObject;
+import com.example.demo.domain.service.kakao.message.json.feed.SendMessageFeedParam;
+import com.example.demo.domain.service.kakao.message.json.list.ListObject;
+import com.example.demo.domain.service.kakao.message.json.list.SendMessageListParam;
+import com.example.demo.domain.service.kakao.message.json.location.SendMessageLocationParam;
+import com.example.demo.domain.service.kakao.message.json.text.SendMessageTextParam;
 import com.example.demo.domain.entity.Friend;
 import com.example.demo.domain.entity.User;
 import com.example.demo.domain.repository.FriendRepository;
 import com.example.demo.domain.service.kakao.KakaoService;
+import com.example.demo.domain.service.kakao.message.KakaoMessageService;
+import com.example.demo.domain.service.kakao.message.json.text.TextObject;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,15 +29,47 @@ public class MessageApiController {
   private final KakaoService kakaoService;
   private final FriendRepository friendRepository;
   private final ModelMapper modelMapper;
+  private final KakaoMessageService kakaoMessageService;
+
+  @PostMapping("/send/text")
+  public HttpEntity<?> sendTextMessage(User user, @RequestBody SendMessageTextParam sendMessageTextParam) {
+    List<String> receiver = sendMessageTextParam.getReceiver();
+    TextObject textObject = sendMessageTextParam.getTextObject();
+
+    kakaoMessageService.sendMessage(user, receiver, textObject);
+    return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("/send/feed")
+  public HttpEntity<?> sendFeedMessage(User user, @RequestBody SendMessageFeedParam sendMessageFeedParam) {
+    List<String> receiver = sendMessageFeedParam.getReceiver();
+    FeedObject feedObject = sendMessageFeedParam.getFeedObject();
+
+    kakaoMessageService.sendMessage(user, receiver, feedObject);
+    return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("/send/list")
+  public HttpEntity<?> sendListMessage(User user, @RequestBody SendMessageListParam sendMessageListParam) {
+    List<String> receiver = sendMessageListParam.getReceiver();
+    ListObject listObject = sendMessageListParam.getListObject();
+
+    kakaoMessageService.sendMessage(user, receiver, listObject);
+    return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("/send/location")
+  public HttpEntity<?> sendLocationMessage(User user, @RequestBody SendMessageLocationParam sendMessageLocationParam) {
+    List<String> receiver = sendMessageLocationParam.getReceiver();
+    ListObject locationObject = sendMessageLocationParam.getListObject();
+
+    kakaoMessageService.sendMessage(user, receiver, locationObject);
+    return ResponseEntity.ok().build();
+  }
+
   @GetMapping("/friend")
   public List<FriendDto> getFriendList (User user) {
     List<Friend> friendList = friendRepository.findByUserId(user.getId());
-    List<FriendDto> result = modelMapper.map(friendList, new TypeToken<List<FriendDto>>() {}.getType());
-
-    for (FriendDto friendDto : result) {
-      System.out.println(friendDto);
-    }
-
-    return result;
+    return modelMapper.map(friendList, new TypeToken<List<FriendDto>>() {}.getType());
   }
 }
