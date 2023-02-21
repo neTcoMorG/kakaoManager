1d9e7d4516764797175ff0553ff2d41cffd202e6
최준혁 <cwnsugr13579@gmail.com>
2023-02-21 14:45:51 +0900

    생성자 수정

--- src/main/java/com/example/demo/domain/service/kakao/message/json/text/TextObject.java
+++ src/main/java/com/example/demo/domain/service/kakao/message/json/text/TextObject.java
@@ -2,16 +2,18 @@ package com.example.demo.domain.service.kakao.message.json.text;
 
 import com.example.demo.domain.service.kakao.message.json.common.Link;
 import com.example.demo.domain.service.kakao.message.json.common.MessageObject;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Map;
-
 @Data
-@AllArgsConstructor
 public class TextObject extends MessageObject {
-    private String object_type;
     private String text;
     private Link link;
     private String button_title;
+
+    public TextObject(String object_type, String text, Link link, String button_title) {
+        super.object_type = object_type;
+        this.text = text;
+        this.link = link;
+        this.button_title = button_title;
+    }
 }
